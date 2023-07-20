ec25a2912e23decee965c01615845d86514fdf68
Zaurall <zaural.mach@gmail.com>
2023-07-20 15:57:26 +0300

    lint

--- src/routes/comic/+page.ts
+++ src/routes/comic/+page.ts
@@ -3,8 +3,8 @@ import type { Comic } from '$lib/components/comic';
 export async function load({ fetch }: { fetch: typeof window.fetch }) {
 	const email = 'n.zinnatullin@innopolis.university';
 
-	const id: number = await fetch('https://fwd.innopolis.university/api/hw2?email=' + email).then((r) =>
-		r.json()
+	const id: number = await fetch('https://fwd.innopolis.university/api/hw2?email=' + email).then(
+		(r) => r.json()
 	);
 
 	const data: Comic = await fetch('https://fwd.innopolis.university/api/comic?id=' + id).then((r) =>
