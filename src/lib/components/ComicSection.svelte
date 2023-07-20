f585c04649a706fcfb2c1dfd809380710250cb06
Zaurall <zaural.mach@gmail.com>
2023-07-20 12:58:21 +0300

    New adapter

--- src/lib/components/ComicSection.svelte
+++ src/lib/components/ComicSection.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import moment from 'moment';
 	import type { Comic } from '$lib/components/comic';
+	import { afterUpdate, onMount } from 'svelte';
 
-	const email = 'n.zinnatullin@innopolis.university';
+	export let comicData: Comic;
 
 	let alt = '';
 	let momentFrom = '';
@@ -10,15 +11,9 @@
 	let safe_title = '';
 	let src = '';
 
-	function fetchComicId(email: string): Promise<Comic> {
-		const params = new URLSearchParams();
-		if (email) {
-			params.append('email', email);
-		}
-		return fetch('https://fwd.innopolis.university/api/hw2?' + params.toString()).then((r) =>
-			r.json()
-		);
-	}
+	onMount(() => {
+		handleImg(comicData);
+	});
 
 	function handleImg(comicObj: Comic) {
 		alt = comicObj.alt;
@@ -31,25 +26,23 @@
 		date = dateObj.toLocaleDateString();
 	}
 
-	async function fetchComicImg() {
-		const id = await fetchComicId(email);
-		const comicJson: Comic = await fetch(
-			'https://fwd.innopolis.university/api/comic?id=' + id
-		).then((r) => r.json());
-		handleImg(comicJson);
-	}
+	onMount(() => {
+		handleImg(comicData);
+	});
+	
+	afterUpdate(() => {
+		handleImg(comicData);
+	});
 </script>
 
-{#await fetchComicImg() then}
-	{#if alt}
-		<section class="section-img">
-			<img class="main-img" {src} {alt} />
-			<p>{safe_title}</p>
-			<p>{date}</p>
-			<p>Published {momentFrom}</p>
-		</section>
-	{/if}
-{/await}
+{#if alt}
+	<section class="section-img">
+		<img class="main-img" {src} {alt} />
+		<p>{safe_title}</p>
+		<p>{date}</p>
+		<p>Published {momentFrom}</p>
+	</section>
+{/if}
 
 <style>
 	.section-img {
--- src/routes/comic/+page.ts
+++ src/routes/comic/+page.ts
@@ -1,5 +1,17 @@
-export function load() {
+import type { Comic } from "$lib/components/comic";
+
+export async function load({ fetch }: { fetch: typeof window.fetch }) {
+	const email = 'n.zinnatullin@innopolis.university';
+
+	const id = await fetch('https://fwd.innopolis.university/api/hw2?email=' + email).then((r) => r.json());
+
+	const data: Comic = await fetch('https://fwd.innopolis.university/api/comic?id=' + id).then((r) => r.json());
+	
+	console.log(1234);
+
 	return {
-		title: 'Comic'
+		title: 'Comic',
+		comicData: data,
 	};
 }
+  
